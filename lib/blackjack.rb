9cf1af508409193f8a6784ac3fc749750e90c241
clever-minifier-2555 <alisabaj@gmail.com>
2019-09-24 16:17:02 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,53 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1...11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  result1 = deal_card
+  result2 = deal_card
+  display_card_total(result1 + result2)
+  result1+result2
 end
 
-def hit?
-  # code hit? here
+def hit?(current_total)
+  prompt_user
+  reply = get_user_input
+  if reply === "s"
+    current_total
+  elsif reply === "h"
+    new_card = deal_card
+    current_total += new_card
+    current_total
+  else
+    invalid_command
+    prompt_user
+    get_user_input
+    current_total
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +55,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  first_cards = initial_round
+  current = hit?(first_cards)
+  display_card_total(current)
+  until current > 21
+    hit?(current)
+  end
+  end_game(current)
 end
-    
